0a9eb59b4b04f01d9448487b76e3b66c4162a3c5
NAKAGAKI Kenji <nakaken0629@gmail.com>
2015-10-25 17:48:49 +0900

    Initial Commit

---
+++ ExpandTableViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpandTableViewSample
+//
+//  Created by 中垣 健志 on 2015/10/25.
+//  Copyright © 2015年 itvirtuoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
