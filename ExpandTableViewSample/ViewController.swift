b391ab5f5de25ff13cb37e4368fb1246a4ecb924
NAKAGAKI Kenji <nakaken0629@gmail.com>
2015-10-25 18:20:08 +0900

    セクション付きのリストビューを表示する処理を作成

--- ExpandTableViewSample/ViewController.swift
+++ ExpandTableViewSample/ViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "section \(section + 1)"
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 8
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("datas")!
+        
+        cell.textLabel!.text = "Swift \(indexPath.section + 1)-\(indexPath.row + 1)"
+        
+        return cell
+    }
 }
 
